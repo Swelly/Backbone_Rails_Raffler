75ea3de6eb15a605d222b55cf7ff135eeab746e0
Wesley Carr <surfswell17@gmail.com>
2013-08-16 10:46:30 -0400

    bg image and render model for entriesIndex

--- app/assets/javascripts/views/entries/entries_index.js
+++ app/assets/javascripts/views/entries/entries_index.js
@@ -1,7 +1,12 @@
 // Template will now be formatted in Handlebars after adding the gem and renaming
 // templates/entries/index.hbs
+
 Raffler.Views.EntriesIndex = Backbone.View.extend({
 
-  template: HandlebarsTemplates['entries/index']
+  template: HandlebarsTemplates['entries/index'],
+  render: function () {
+    $(this.el).html(this.template());
+    return this;
+  }
 
 });
