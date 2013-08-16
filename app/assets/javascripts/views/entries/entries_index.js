cbee1994fe97d3e8ed44b0ddd15d436a732857d4
Wesley Carr <surfswell17@gmail.com>
2013-08-16 10:22:08 -0400

    Update before push

--- app/assets/javascripts/views/entries/entries_index.js
+++ app/assets/javascripts/views/entries/entries_index.js
@@ -1,3 +1,5 @@
+// Template will now be formatted in Handlebars after adding the gem and renaming
+// templates/entries/index.hbs
 Raffler.Views.EntriesIndex = Backbone.View.extend({
 
   template: HandlebarsTemplates['entries/index']
