9602aec510e79aad78a7564b38bed1b1fa85fded
Wesley Carr <surfswell17@gmail.com>
2013-08-16 10:10:29 -0400

    Backbone installed and functional

--- app/assets/javascripts/raffler.js
+++ app/assets/javascripts/raffler.js
@@ -1,3 +1,8 @@
+// Generated from rails g backbone:install --javascript
+// - as well as all directories in this javascripts fldr
+
+
+// Raffler bound to window with these Models - Collections, Views and Routers.
 window.Raffler = {
   Models: {},
   Collections: {},
