f716bc457daa1d7d4aa487d10ac3a520e8624dc2
Wesley Carr <surfswell17@gmail.com>
2013-08-16 10:33:07 -0400

    Made a method of show for each entry :id in the Routers.js

--- app/assets/javascripts/routers/entries_router.js
+++ app/assets/javascripts/routers/entries_router.js
@@ -1,11 +1,15 @@
 Raffler.Routers.Entries = Backbone.Router.extend({
 
   routes: {
-    '': 'index'
+    '': 'index',
+    'entries/:id': 'show'
   },
-
+  // "Methods" (type of functions within an Object - Raffler)
   index: function () {
-    alert("Hello from index!");
+    alert("Hello from Index!");
+  },
+  show: function (id) {
+    alert("Hello from Show Number: " + id); // Ex: http://localhost:3000/#entries/1
   }
 
 });
